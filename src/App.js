8729d1a270a184458a44042749cf28360c2e7e2c
patrickmedaugh <patrickmedaugh@gmail.com>
2016-12-08 12:46:01 -0700

    Just removed some things from the top header. Saving things as they are working to make changes to the API.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Coin Xchange</h2>
         </div>
             <TopTicker />
