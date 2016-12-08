e60dc5127af8831e3cafbb8b0289b8c8464f0a23
patrickmedaugh <patrickmedaugh@gmail.com>
2016-12-08 13:18:21 -0700

    Adds chart

---
+++ src/Chart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Chart.css';
+
+class Chart extends Component {
+  render() {
+    return (
+      <div className="Chart">
+        Chart goes here
+      </div>
+    )
+  }
+}
+
+export default Chart;
